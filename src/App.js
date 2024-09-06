bc562c9ee535401207b669b66f57343bb60e70bd
isha3024 <isha.gandhi3024@gmail.com>
2024-09-06 16:37:54 +0530

    Notes Taking App using React JS

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import { Navbar } from './components';
+
+export const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './styles.css'
+
+export const Navbar = () => {
+  return (
+    <div className='container'>Navbar</div>
+  )
+}
